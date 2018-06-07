8bbbb47bd0d4b1fc211a54299188d2ae7a743190
Brian <rac3k@aol.com>
2018-06-07 13:12:39 -0400

    CSS styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,6 @@ $('#goal').text('Goal: '+targetScore);
 console.log(targetScore);
 
 var currentScore = 0;
-// gem scores should be betwwen 1 - 12.
-var blackGem=Math.floor(Math.random()*11+1);
-var blueGem=Math.floor(Math.random()*11+1);
-var purpleGem=Math.floor(Math.random()*11+1);
-var redGem=Math.floor(Math.random()*11+1);
-
 var wins = 0;
 var losses = 0;
 
@@ -32,7 +26,7 @@ function reset() {
     targetScore=Math.floor((Math.random()*101)+19);
     $('#goal').text('Goal: '+targetScore);
 
-    blackgem=Math.floor((Math.random()*11)+1);
+    blackGem=Math.floor((Math.random()*11)+1);
     blueGem=Math.floor((Math.random()*11)+1);
     purpleGem=Math.floor((Math.random()*11)+1);
     redGem=Math.floor((Math.random()*11)+1);
