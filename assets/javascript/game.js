bf464c09dfd773767a8387785751372fcdf8fce7
Brian <rac3k@aol.com>
2018-06-07 12:49:17 -0400

    functions of wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,14 +12,26 @@ var purpleGem=Math.floor(Math.random()*11+1);
 var redGem=Math.floor(Math.random()*11+1);
 
 var wins = 0;
-$('#wins').text('Wins: '+ wins);
 var losses = 0;
-$('#losses').text('Losses: '+ losses);
+
+function won(){
+    alert('Congratulations!');
+    wins++;
+    $('#wins').text('Wins: '+wins);
+    reset ();
+}
+
+function lost(){
+    alert('Sorry try again.')
+    losses++;
+    $('#losses').text('Losses: '+losses);
+    reset();
+}
 
 function reset() {
     targetScore=Math.floor((Math.random()*101)+19);
     $('#goal').text('Goal: '+targetScore);
-    
+
     blackgem=Math.floor((Math.random()*11)+1);
     blueGem=Math.floor((Math.random()*11)+1);
     purpleGem=Math.floor((Math.random()*11)+1);
@@ -27,11 +39,50 @@ function reset() {
     currentScore = 0;
     $('#userScore').text(currentScore);
 }
+
 reset();
 $('.redGem').on('click', function(){
     currentScore = currentScore+redGem;
     console.log(currentScore);
-    $('#userScore').text(currentScore);
+    $('#userScore').text('Current Score: '+currentScore);
+        if (currentScore == targetScore){
+            won();
+        } else if (currentScore>targetScore){
+            lost();
+        }
+})
+
+$('.blackGem').on('click', function(){
+    currentScore = currentScore+blackGem;
+    console.log(currentScore);
+    $('#userScore').text('Current Score: '+currentScore);
+        if (currentScore == targetScore){
+            won();
+        } else if (currentScore>targetScore){
+            lost();
+        }
+})
+
+$('.blueGem').on('click', function(){
+    currentScore = currentScore+blueGem;
+    console.log(currentScore);
+    $('#userScore').text('Current Score: '+currentScore);
+        if (currentScore == targetScore){
+            won();
+        } else if (currentScore>targetScore){
+            lost();
+        }
+})
+
+$('.purpleGem').on('click', function(){
+    currentScore = currentScore+purpleGem;
+    console.log(currentScore);
+    $('#userScore').text('Current Score: '+currentScore);
+        if (currentScore == targetScore){
+            won();
+        } else if (currentScore>targetScore){
+            lost();
+        }
 })
 
 });
