5a085dc1fa81a1bc08e6f14f0b9af49218f0b39f
Brian <rac3k@aol.com>
2018-06-06 22:06:08 -0400

    reset function and var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
+//number should start between 19 - 120.
+var targetScore = Math.floor(Math.random()*101+19);
+$('#goal').text('Goal: '+targetScore);
+console.log(targetScore);
 
-var targetScore = 0;
 var currentScore = 0;
-var blackGem = 0;
-var blueGem = 0;
-var purpleGem = 0;
-var redGem = 0;
+// gem scores should be betwwen 1 - 12.
+var blackGem=Math.floor(Math.random()*11+1);
+var blueGem=Math.floor(Math.random()*11+1);
+var purpleGem=Math.floor(Math.random()*11+1);
+var redGem=Math.floor(Math.random()*11+1);
+
 var wins = 0;
+$('#wins').text('Wins: '+ wins);
 var losses = 0;
-var gameStatus = true;
+$('#losses').text('Losses: '+ losses);
 
 function reset() {
-    //number should start between 19 - 120.
     targetScore=Math.floor((Math.random()*101)+19);
-    console.log(targetScore)
-    // gem scores should be betwwen 1 - 12.
+    $('#goal').text('Goal: '+targetScore);
+    
     blackgem=Math.floor((Math.random()*11)+1);
     blueGem=Math.floor((Math.random()*11)+1);
     purpleGem=Math.floor((Math.random()*11)+1);
     redGem=Math.floor((Math.random()*11)+1);
     currentScore = 0;
-
+    $('#userScore').text(currentScore);
 }
 
 });
